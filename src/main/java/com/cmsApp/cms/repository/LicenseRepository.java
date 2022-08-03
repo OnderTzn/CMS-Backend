2379c91fea79eb1a50417e1e5d35d54c1038e395
onder tozan <ondertozann@hotmail.com>
2022-08-03 18:06:05 +0300

    Query method for deleting a license is added

--- src/main/java/com/cmsApp/cms/repository/LicenseRepository.java
+++ src/main/java/com/cmsApp/cms/repository/LicenseRepository.java
@@ -2,12 +2,19 @@ package com.cmsApp.cms.repository;
 
 import com.cmsApp.cms.model.License;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
 
 public interface LicenseRepository extends JpaRepository<License, Long> {
 
-    void deleteLicenseById(Long id);
+    //void deleteLicenseById(Long id);
 
     License findLicenseById(Long licenseId);    //Optional<License> findLicenseById(Long licenseId)
-
-
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM content_licenses_of_content WHERE license_id = :licenseId", nativeQuery = true)
+    void deleteByLicenseId(@Param("licenseId") Long licenseId);
 }
